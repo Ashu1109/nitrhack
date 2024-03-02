5f25ca22d13e55b92ecac84f46091a5b8f8c2533
Aayush kumar <aayushkumarhigh@gmail.com>
2024-03-02 18:52:28 +0530

    home

--- app/(user)/dashboard/page.jsx
+++ app/(user)/dashboard/page.jsx
@@ -1,6 +1,8 @@
+import Blog from "@/components/Home/Blog";
 import Carosel from "@/components/Home/Carosel";
 import Home from "@/components/Home/Home";
 import Service from "@/components/Home/Service";
+import Footer from "@/components/Home/footer";
 
 const page = () => {
   return (
@@ -8,6 +10,8 @@ const page = () => {
       <Home />
       <Service />
       <Carosel />
+      <Blog />
+      <Footer />
     </>
   );
 };
