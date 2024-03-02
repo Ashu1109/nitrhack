bb3523f50b11f72906050021d938dda5065c61d8
Sachinkumar3942 <sachinkumar3942.9999@gmail.com>
2024-03-02 19:00:53 +0530

    update

---
+++ app/profile/Piechart.js
@@ -0,0 +1,37 @@
+"use client";
+import React from 'react';
+import 'chart.js/auto';
+import { Pie } from 'react-chartjs-2';
+
+const PieChart = () => {
+  // Data for the pie chart
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green'],
+    datasets: [
+      {
+        data: [300, 50, 100, 150],
+        backgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56',
+          '#4BC0C0'
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56',
+          '#4BC0C0'
+        ]
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <h2>Pie Chart</h2>
+      <Pie className=' w-[20dvw] h-[20dvw]' data={data}  />
+    </div>
+  );
+};
+
+export default PieChart;
--- app/profile/page.js
+++ app/profile/page.js
@@ -1,14 +1,155 @@
 "use client";
 import Image from "next/image";
-import ProfileImg from "./uploads/profile.jpg"
+// import file1 from "./uploads/file.pdf"
+import { FaPhone } from "react-icons/fa";
+import ProfileImg from "./uploads/profile.jpg";
+import { IoMdHome } from "react-icons/io";
+import { FaFile } from "react-icons/fa6";
+import { HiOutlineMail } from "react-icons/hi";
+import { RxCross2 } from "react-icons/rx";
+import { useState } from "react";
+import PieChart from "./Piechart"
 const Profile = () => {
-  return (
-    <div className="flex bg-gradient-to-r from-cyan-300 to-blue-400 max-h-full">
-      <div className=" w-40 flex-1 items-center p-2 h-full  bg-blue-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-gray-100 ">
-      <Image src={ProfileImg} className=" rounded-full items-center justify-center  mt-5" width={100} height={50}/>
-      <h2 className=" font-bold text-lg">Sachin kumar</h2>
+
+    
+  const [display, setDisplay] = useState(false);
+  
+  const arr = Array(10).fill("abc3");
+  
+  if (display) {
+    return (
+      <div className=" h-[100dvh] flex-col bg-gradient-to-r flex gap-3  from-cyan-400 to-emerald-500">
+        <div className=" flex justify-between items-center w-full ">
+          <span className=" text-3xl pl-3 font-bold text-blue-700">
+            All pdfs
+          </span>
+          <button
+            type="button"
+            onClick={() => {
+              setDisplay(false);
+            }}
+            className="text-white bg-gradient-to-r from-blue-500 flex gap-2 mt-2 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            <RxCross2 />
+            Close
+          </button>
+        </div>
+        <div className=" pt-4 pl-2">
+          {arr.map((file, index) => (
+            <div key={index} className=" flex flex-col p-2">
+              <div class="flex gap-2">
+                <FaFile />
+                <h2>{arr[index]}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className=" w-[80vw]   ">sir</div>
+    );
+  }
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-5xl text-blue-500 p-5 font-extrabold">
+        Patient details
+      </h1>
+      <div className="flex">
+        <div className="flex p-4 flex-col gap-2 ">
+          <div className=" p-4 border-2 border-black rounded-xl ">
+            <div className="flex gap-4 text-center justify-center items-center">
+              <Image
+                className=" overflow-hidden"
+                src={ProfileImg.src}
+                width={100}
+                height={100}
+              />
+              <h1 className="text-xl font-bold">Sachin kumar</h1>
+            </div>
+            <h1 className="text-xl pt-4 font-bold">Contact details :</h1>
+            <div className="flex pt-1 items-center gap-3">
+              <FaPhone className="text-2xl text-cyan-400" />
+              <h1 className="text-xl font-semibold text-gray-400">
+                7894568231
+              </h1>
+            </div>
+            <div className="flex pt-1 items-center gap-3">
+              <HiOutlineMail className=" text-2xl font-extrabold text-cyan-400" />
+              <h1 className="text-xl font-semibold text-gray-400">
+                sachin@123.com
+              </h1>
+            </div>
+            <div className="flex pt-1 items-center gap-3">
+              <IoMdHome className=" text-2xl font-extrabold text-cyan-400" />
+              <div className="flex flex-col">
+                <h1 className="text-xl font-semibold text-gray-400">
+                  Adityapur,
+                </h1>
+                <h1 className="text-xl font-semibold text-gray-400">
+                  Jamshedpur,Jharkhand
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className=" p-4 border-2 border-black rounded-xl ">
+            <h1 className="text-3xl pb-2 pt-4 font-bold">Latest results : </h1>
+            <div className="flex pt-1 items-center gap-3">
+              <FaFile className="text-2xl text-cyan-400" />
+              <h1 className="text-xl font-semibold text-gray-800">test.pdf</h1>
+            </div>
+            <div className="flex pt-1 items-center gap-3">
+              <FaFile className="text-2xl text-cyan-400" />
+              <h1 className="text-xl font-semibold text-gray-800">test2.pdf</h1>
+            </div>
+            <div className="flex pt-4 items-center gap-3">
+              
+              <button
+                type="button"
+                onClick={() => setDisplay(true)}
+                className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                View All
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full p-4 flex-col gap-2 ">
+          <div className=" p-4 w-full border-2 border-black rounded-xl ">
+            <h1 className="text-xl pt-4 font-bold">Overview :</h1>
+            <div className="flex flex-col  pt-1 items-center gap-3">
+              <div className="flex justify-between w-full items-center">
+                <div className="flex flex-col">
+                  <h1 className=" text-gray-800 text-xl">Gender:</h1>
+                  <span className=" text-gray-400 text-md">Male</span>
+                </div>
+                <div className="flex flex-col">
+                  <h1 className=" text-gray-800 text-xl">Age:</h1>
+                  <span className=" text-gray-400 text-md">20 yrs</span>
+                </div>
+                <div className="flex flex-col">
+                  <h1 className=" text-gray-800 text-xl">Semester:</h1>
+                  <span className=" text-gray-400 text-md">1st </span>
+                </div>
+              </div>
+              <div className="flex justify-between w-full items-center">
+                <div className="flex flex-col">
+                  <h1 className=" text-gray-800 text-xl">Last visit</h1>
+                  <span className=" text-gray-400 text-md">28:02:2024</span>
+                </div>
+                <div className="flex flex-col">
+                  <h1 className="text-gray-800 text-xl">Next visit</h1>
+                  <span className=" text-gray-400 text-md">2:03:2024</span>
+                </div>
+                <div className="flex flex-col">
+                  <h1 className=" text-gray-800 text-xl">Problem:</h1>
+                  <span className=" text-gray-400 text-md">Depression </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" p-4 border-2 border-black rounded-xl ">
+            <PieChart/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
