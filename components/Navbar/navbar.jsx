74901a3a928d5f5d8fca8b208380e83423d21556
Aayush kumar <aayushkumarhigh@gmail.com>
2024-03-02 13:44:07 +0530

    flf

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -4,26 +4,37 @@ import Link from "next/link"
 import { Button } from "../ui/button";
 const Navbar = () => {
     return (
-        <div className={"  bg-[#2770e6] text-black  h-[10vh] flex justify-between items-center px-10"}>
-            <div className={" flex gap-3 text-slate-300 items-center text-xl "}><MdLocalHospital color="red" size={40} />Hack4Health</div>
-            <div className={" flex gap-3"}>
-                <Link href={"/"}>
-                    <Button variant="secondary">Home</Button>
-                </Link>
-                <Link href={"/"}>
-                    <Button variant="secondary">About</Button>
-                </Link>
-                <Link href={"/"}>
-                    <Button variant="secondary">Contact</Button>
-                </Link>
-                <Link href={"/"}>
-                    <Button variant="secondary">History</Button>
-                </Link>
-                <Link href={"/"}>
-                    <Button className={" uppercase"} variant="link-plain">Make Appointment</Button>
-                </Link>
-            </div>
+      <div
+        className={
+          "  bg-[#2770e6] text-black  h-[10vh] flex justify-between items-center px-10"
+        }
+      >
+        <div className={" flex gap-3 text-slate-300 items-center text-xl "}>
+          <MdLocalHospital color="red" size={40} />
+          Hack4Health
         </div>
+        <div className={" flex gap-3 "}>
+          <div className={"px-10 flex gap-3 "}>
+            <Link href={"/"}>
+              <Button variant="secondary">Home</Button>
+            </Link>
+            <Link href={"/"}>
+              <Button variant="secondary">About</Button>
+            </Link>
+            <Link href={"/"}>
+              <Button variant="secondary">Contact</Button>
+            </Link>
+            <Link href={"/"}>
+              <Button variant="secondary">History</Button>
+            </Link>
+          </div>
+          <Link href={"/"}>
+            <Button className={" uppercase"} variant="link-plain">
+              Make Appointment
+            </Button>
+          </Link>
+        </div>
+      </div>
     );
 };
 
