c43c4bd6c606000ec89fe8db91e06eec5ef58c54
Sachinkumar3942 <sachinkumar3942.9999@gmail.com>
2024-03-23 16:49:35 +0530

    update

--- app/doctors/[id]/page.js
+++ app/doctors/[id]/page.js
@@ -8,6 +8,8 @@ import { MdOutlineCastForEducation } from "react-icons/md";
 import { LuIndianRupee } from "react-icons/lu";
 import { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
+import toast from "react-hot-toast";
+
 const stripePromise = loadStripe("your_publishable_key");
 
 const handlePayment = async () => {
@@ -34,7 +36,9 @@ const handlePayment = async () => {
     }
   }
 };
-
+const handleClick=()=>{
+  alert("Appointment Successful");
+}
 import { PiRocket } from "react-icons/pi";
 const Doctor = ({ params }) => {
   const { id } = params;
@@ -190,15 +194,12 @@ const Doctor = ({ params }) => {
         </div>
 
         <button
-          onClick={handlePayment}
+         onClick={handleClick}
           className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg bg-gradient-to-r from-indigo-400 to-cyan-400"
         >
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 dark:bg-gray-900 rounded-md hover:bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          <span  className="relative px-5 py-2.5 transition-all ease-in duration-75 dark:bg-gray-900 rounded-md hover:bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
             Pay Now
-            <div className="flex items-center">
-              <LuIndianRupee />
-              {arr[id].price}
-            </div>
+            
           </span>
         </button>
       </div>
